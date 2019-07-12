504d2c4783ba3b7d09564a22d95fe5a085f0f945
Rafal Hryniewski <rafal@hryniewski.net>
2019-07-12 09:39:09 +0200

    Alerts monitor

--- Bimbrownia.AI.AlertsMonitor/Program.cs
+++ Bimbrownia.AI.AlertsMonitor/Program.cs
@@ -1,5 +1,9 @@
-﻿using Confluent.Kafka;
+﻿using Bimbrownia.AI.Shared;
+using Confluent.Kafka;
+using Newtonsoft.Json;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Bimbrownia.AI.AlertsMonitor
 {
@@ -12,9 +16,99 @@ namespace Bimbrownia.AI.AlertsMonitor
             AutoOffsetReset = AutoOffsetReset.Earliest
         };
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Alerts Monitor";
+            Console.WriteLine($"----------BIMBROWNIA AI----------{Environment.NewLine}{Environment.NewLine}{Environment.NewLine}");
+
+            Task.Run(MonitorStillStarted);
+            Task.Run(MonitorStillDisabled);
+
+            Console.ReadLine();
+        }
+
+        private static void MonitorStillStarted()
+        {
+            using (var c = new ConsumerBuilder<Ignore, string>(ConsumerConfig).Build())
+            {
+                c.Subscribe(Constants.TopicName_StillStarted);
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+
+                            var ev = JsonConvert.DeserializeObject<StillStartedEvent>(cr.Value);
+                            HandleStillStartedEvent(ev);
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+
+        private static void MonitorStillDisabled()
+        {
+            using (var c = new ConsumerBuilder<Ignore, string>(ConsumerConfig).Build())
+            {
+                c.Subscribe(Constants.TopicName_StillDisabled);
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+
+                            var ev = JsonConvert.DeserializeObject<StillDisabledEvent>(cr.Value);
+                            HandleStillDisabledEvent(ev);
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+
+        private static void HandleStillStartedEvent(StillStartedEvent ev)
+        {
+            Misc.WriteLineInColor($"{DateTime.Now:yyyy/MM/dd hh:mm:ss.fff}: Still with id {ev.StillId} started at {ev.Occured:yyyy/MM/dd hh:mm:ss.fff}", ConsoleColor.Green);
+        }
+
+        private static void HandleStillDisabledEvent(StillDisabledEvent ev)
+        {
+            Misc.WriteLineInColor($"{DateTime.Now:yyyy/MM/dd hh:mm:ss.fff}: Still with id {ev.StillId} disabled at {ev.Occured:yyyy/MM/dd hh:mm:ss.fff}", ConsoleColor.Red);
         }
     }
 }
---
+++ Bimbrownia.AI.Shared/Misc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bimbrownia.AI.Shared
+{
+    public static class Misc
+    {
+        public static void WriteLineInColor(string msg, ConsoleColor color)
+        {
+            var prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(msg);
+
+            Console.ForegroundColor = prevColor;
+
+        }
+    }
+}
